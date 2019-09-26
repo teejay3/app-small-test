6de197790f263b24bd467f23e92340481fb31e1b
teejay3 <34775293+teejay3@users.noreply.github.com>
2019-09-26 09:47:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  res.end(`<h1>Hello World</h1><h2>${process.env.PORT}</h2>`);
 });
 
 server.listen(port,() => {
